e3d629209f071c3f0a77df9890f926898fec24ca
ranjit singh <ranjitsingh@ranjits-Mac-mini-2.local>
2017-05-02 12:22:30 +0530

    Initial Commit

---
+++ Vchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vchat
+//
+//  Created by ranjit singh on 5/2/17.
+//  Copyright © 2017 ranjit singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
